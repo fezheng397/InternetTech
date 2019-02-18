603d5f91b50d88cb3cab3fa8796ae0a133691601
JasonG7234 <JasonG7234@gmail.com>
2019-02-17 20:10:49 -0500

    Client almost connects to ts

--- project1/client.py
+++ project1/client.py
@@ -4,7 +4,7 @@ import random
 import sys, os
 import socket
 
-def client(rsHostName, rsListenPort, hostNames):
+def client(rsHostName, rsListenPort, hostNames, rsConnection):
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
@@ -19,18 +19,45 @@ def client(rsHostName, rsListenPort, hostNames):
     # connect to the server on local machine
     server_binding = (localhost_addr, port)
     cs.connect(server_binding)
-
-    for host in hostNames: 
-      # Send data to the server
-      data_to_server=cs.send(host.encode('utf-8'))
-      print("[C]: Data sent to server: {}".format(host))
-
-	    #Receive data from the server
-      data_from_server=cs.recv(100)
-      msg_rcv = data_from_server.decode('utf-8')
-      print("[C]: Data received from server: {}".format(msg_rcv))
-
+    
+    # send and receive data
+    if rsConnection:
+        interpretRSConnection(cs, hostNames)
+    else:
+        outputResults(cs, hostNames)
+    
     # close the client socket
+    cs.close()
+    exit()
+    
+def interpretRSConnection(cs, hostNames):
+    for host in hostNames: 
+        # Send data to the server
+        data_to_server=cs.send(host.encode('utf-8'))
+        print("[C]: Data sent to server: {}".format(host))
+
+	      #Receive data from the server
+        data_from_server=cs.recv(100)
+        msg_rcv = data_from_server.decode('utf-8')
+        print("[C]: Data received from server: {}".format(msg_rcv))
+        if msg_rcv is 'END':
+            break
+        elif msg_rcv[-2:] == 'NS':
+            print(msg_rcv[:-5])
+            tsHostName = msg_rcv[:-5]
+            client(tsHostName, sys.argv[3], [host, 'END'], False)
+            
+def outputResults(cs, hostNames):
+    for host in hostNames: 
+        # Send data to the server
+        data_to_server=cs.send(host.encode('utf-8'))
+        print("[C]: Data sent to server: {}".format(host))
+
+	      #Receive data from the server
+        data_from_server=cs.recv(100)
+        msg_rcv = data_from_server.decode('utf-8')
+        print("[C]: Data received from server: {}".format(msg_rcv))
+        
     cs.close()
     exit()
     
@@ -45,14 +72,14 @@ def getAllHostNamesFromFile():
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) is not 3 or not sys.argv[2].isdigit()):
+    if (len(sys.argv) is not 4 or not sys.argv[2].isdigit() or not sys.argv[3].isdigit()):
         print("Please enter valid parameter syntax")
         sys.exit(0)
         
     hostNames = getAllHostNamesFromFile()
   
     time.sleep(random.random() * 5)
-    t2 = threading.Thread(name='client', target=client, args=([sys.argv[1], sys.argv[2], hostNames]))
+    t2 = threading.Thread(name='client', target=client, args=([sys.argv[1], sys.argv[2], hostNames, True]))
     t2.start()
 
     time.sleep(15)
