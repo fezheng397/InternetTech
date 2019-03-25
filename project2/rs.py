20e9156e614d88b544147368f5f41642a465b944
fezheng397 <fezheng397@gmail.com>
2019-03-25 15:12:08 -0400

    rsServer changes

--- project2/rs.py
+++ project2/rs.py
@@ -4,7 +4,7 @@ import random
 import sys, os
 import socket
 
-def server(rsListenPort, rsConnections, tsHostname):
+def server(rsListenPort, rsConnections, tsHostname, tsEduListenPort, tsComListenPort):
     print(rsListenPort)
 
     try:
@@ -45,7 +45,24 @@ def server(rsListenPort, rsConnections, tsHostname):
         if connectionValues != None:
             msg = data_from_server + ' ' + connectionValues[0] + ' ' + connectionValues[1]
         else:
-            msg = tsHostname
+            #msg = tsHostname
+            #Check for data_from_server ending block
+            temp = data_from_server.split('.')
+            tempLen = len(temp)
+            #If end block is com - direct to tsCOM
+            if temp[tempLen - 1] == 'com':
+                print 'com'
+                clientTS(rsConnections.get('tsCom'), tsComListenPort, data_from_server, msg)
+                #msg = 'tsCOM'
+            #else if end block is edu - direct to tsEDU
+            elif temp[tempLen - 1] == 'edu':
+                print 'edu'
+                clientTS(rsConnections.get('tsEdu'), tsEduListenPort, data_from_server, msg)
+                #msg = 'tsEDU'
+            #else return error msg
+            else:
+                msg = data_from_server + ' - Error:HOST NOT FOUND'
+
         csockid.send(msg.encode('utf-8'))
         
 
@@ -53,8 +70,41 @@ def server(rsListenPort, rsConnections, tsHostname):
     ss.close()
     exit()
 
-def reverse(msg):
-	return msg[::-1]
+def clientTS(tsHostName, tsListenPort, hostname, msg_rcv):
+    try:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+        
+    # Define the port on which you want to connect to the server
+    port = int(rsListenPort)
+    localhost_addr = tsHostName
+
+    # connect to the server on local machine
+    server_binding = (localhost_addr, port)
+    try:
+        cs.connect(server_binding)
+    except Exception as e:
+        print(e)
+        cs.close()
+        exit()
+        
+    # send and receive data
+    # Send data to the server
+    data_to_server=cs.send(hostname.encode('utf-8'))
+    print("[C]: Data sent to TServer: {}".format(host))
+
+        #Receive data from the server
+    data_from_server=cs.recv(100)
+    msg_rcv = data_from_server.decode('utf-8')
+    print("[C]: Data received from TServer: {}".format(msg_rcv))
+    
+    # close the client socket
+    cs.close()
+    exit()
+
 
 def readFile(filename):
     fileContent = []
@@ -65,16 +115,26 @@ def readFile(filename):
     return fileContent
 
 def createConnections(rsConnections, fileContent):
-    contentLength = len(fileContent)
-    tsHostname = fileContent[contentLength - 1]
-    fileContent = fileContent[:(contentLength - 1)]
+    #contentLength = len(fileContent)
+    #tsHostname = fileContent[contentLength - 1]
+    #fileContent = fileContent[:(contentLength - 1)]
     for connection in fileContent:
         substrings = connection.split(' ')
-        #print substrings
-        rsConnections[substrings[0]] = (substrings[1], substrings[2])
+        if substrings[2] == 'NS':
+            temp = substrings[0].split('.')
+            print (temp)
+            if(temp[len(temp) - 1] == 'com'):
+                #add TSCOM to rsConnections
+                rsConnections['tsCom'] = (substrings[1], substrings[2])
+            else:
+                #add TSEDU to rsConnections
+                rsConnections['tsEdu'] = (substrings[1], substrings[2])
+        else:
+            #print substrings
+            rsConnections[substrings[0]] = (substrings[1], substrings[2])
     
     #print rsConnections
-    return tsHostname
+    #return tsHostname
 
 
 if __name__ == "__main__":
@@ -97,8 +157,8 @@ if __name__ == "__main__":
 
     tsHostname = createConnections(rsConnections, DNSRSContent)
     print rsConnections
-    
-    t1 = threading.Thread(name='server', target=server, args=(sys.argv[1], rsConnections, tsHostname))
+
+    t1 = threading.Thread(name='server', target=server, args=(sys.argv[1], rsConnections, tsHostname, sys.argv[2], sys.argv[3]))
     t1.start()
 
     time.sleep(15)
